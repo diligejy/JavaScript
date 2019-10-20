89b653aeb5ac8ed657691d3c4f54d963966ef3a6
Jinyoung <40735154+diligejy@users.noreply.github.com>
2019-10-20 15:00:06 +0900

    Add files via upload

---
+++ ZeroCho/JavaScript/WordChain.js
@@ -0,0 +1,11 @@
+var word = '송진영'
+while(true) {
+    var answer = prompt(word)
+    if (word[word.length - 1] === answer[0]){
+		//맞았을 때
+        word = answer;	
+    } else {
+		//틀렸을 때
+    }
+
+}
