b0b528f2d0b85eda27e838eb59c8cc4e034a9b21
diligejy <sjy049@gmail.com>
2020-10-04 07:55:10 +0900

    push

--- Dream_coding/function.js
+++ Dream_coding/function.js
@@ -2,3 +2,10 @@
 // - fundamental building block in the program
 // - subprogram can be used multiple times
 // - performs a task or calculates a value
+
+// 1. Function declaration
+// 2. function name(param1, param2) { body... return;}
+// one function === one thing;
+// naming ; doSomething; command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
