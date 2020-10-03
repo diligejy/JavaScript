13bbc657b39009688193957924c525df1d2e8714
diligejy <sjy049@gmail.com>
2020-10-04 06:04:59 +0900

    Dream coding ch3 variable

--- Dream_coding/variable.js
+++ Dream_coding/variable.js
@@ -1,2 +1,106 @@
+// 1. Use Strict
+// added in ES5
+// use this for Vanilla JavaScript 
 'use strict';
-console.log('Hello World!');
+//console.log('Hello World!');
+
+// 2. Variable
+// let (added in ES6)
+let globalName = 'global name';
+{
+    let name = 'ellie';
+    console.log(name);
+    name = 'hello';
+    console.log(name);
+}
+console.log(globalName);
+
+
+{
+    age = 4;
+    var age;
+}
+
+console.log(age);
+
+// 3. constant
+// favor immutable data type always for a few reasons;
+// - security
+// - thread safety
+// - reduce human mistakes
+const daysInWeek = 7;
+const maxNumber = 5;
+
+
+// 4. Variable Types
+// primitive, single item: number, string, boolean, null, undefined, symbol
+// object, box
+// function, first-class function
+
+const count = 17; // integer
+const size = 17.1; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`)
+console.log(`value: ${size}, type: ${typeof size}`)
+
+// number - speicla numeric values: infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt (fairly new, don't use it yet)
+const bigInt = 1234567890123456789012345678901234567890; // over (-2**53) ~ 2**53
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`)
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`)
+const helloBob = `hi ${brendan}`; // template literals (string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`)
+
+// boolean
+// false: 0, null, undefined, NaN, ''
+// true: any other value
+const canRead = true;
+const test = 3 < 1; 
+console.log(`value: ${canRead}, type: ${typeof canRead}`)
+console.log(`value: ${test}, type: ${typeof test}`)
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`);
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`);
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 === symbol2);
+
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 === gSymbol2); // true
+console.log(`value: ${symbol1.description}, type:${typeof symbol1.description}`)
+
+// object, real-life object, data structure
+const ellie = {name : 'ellie', age: 20};
+ellie.age = 21;
+console.log(ellie.age);
+
+// 5. Dynamic typing: dynamically typed language
+let text = 'hello';
+console.log(text.charAt(0));
+console.log(`value: ${text}, type: ${typeof text}`);
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`);
+text = '8' / '2';
+console.log(`value: ${text}, type: ${typeof text}`);
